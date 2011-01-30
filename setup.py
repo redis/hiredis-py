3bf640069b6d8ded49785b687d0b0fa7a3247c8b
Pieter Noordhuis <pcnoordhuis@gmail.com>
2011-01-30 14:28:06 +0100

    Patch install_lib to run build_clib before build_ext

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from distutils.core import setup, Extension
+from distutils.command import install_lib as _install_lib
 import sys, imp, os, glob
 
 # Make sure vendor/hiredis is checked out (for development)
@@ -12,6 +13,19 @@ def version():
   module = imp.load_source("hiredis.version", "hiredis/version.py")
   return module.__version__
 
+# Patch "install_lib" command to run build_clib before build_ext
+# to properly work with easy_install.
+# See: http://bugs.python.org/issue5243
+class install_lib(_install_lib.install_lib):
+  def build(self):
+    if not self.skip_build:
+      if self.distribution.has_pure_modules():
+        self.run_command('build_py')
+        if self.distribution.has_c_libraries():
+          self.run_command('build_clib')
+        if self.distribution.has_ext_modules():
+          self.run_command('build_ext')
+
 lib = ("hiredis", {
   "sources": ["vendor/hiredis/%s.c" % src for src in ("hiredis", "net", "sds")],
   "include_dirs": ["vendor/hiredis"]})
@@ -22,14 +36,18 @@ ext = Extension("hiredis.hiredis",
   libraries=["hiredis"])
 
 setup(
-    name="hiredis",
-    version=version(),
-    description="Python extension that wraps hiredis",
-    url="https://github.com/pietern/hiredis-py",
-    author="Pieter Noordhuis",
-    author_email="pcnoordhuis@gmail.com",
-    keywords=["Redis"],
-    license="BSD",
-    packages=["hiredis"],
-    libraries=[lib],
-    ext_modules=[ext])
+  name="hiredis",
+  version=version(),
+  description="Python extension that wraps hiredis",
+  url="https://github.com/pietern/hiredis-py",
+  author="Pieter Noordhuis",
+  author_email="pcnoordhuis@gmail.com",
+  keywords=["Redis"],
+  license="BSD",
+  packages=["hiredis"],
+  libraries=[lib],
+  ext_modules=[ext],
+
+  # Override "install_lib" command
+  cmdclass={ "install_lib": install_lib },
+)
