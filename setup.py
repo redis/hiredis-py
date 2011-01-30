f57522438fbce785727aab332d8e8978e395d9fd
Pieter Noordhuis <pcnoordhuis@gmail.com>
2011-01-30 13:43:57 +0100

    Compile hiredis as part of the distutils build command

--- setup.py
+++ setup.py
@@ -1,26 +1,20 @@
+#!/usr/bin/env python
 from distutils.core import setup, Extension
 import sys, os, glob
 
-# Compile hiredis as static library when setup.py is used directly. (I'm aware
-# this is a big hack, but I don't how this is done properly with disttools).
+# Make sure vendor/hiredis is checked out (for development)
 if __name__ == "__main__":
   # Initialize submodule when not present
   if not os.path.isdir("vendor/hiredis"):
     os.system("git submodule update --init")
-  cwd = os.getcwd()
-  os.chdir("vendor/hiredis")
 
-  # Python shipped with OSX needs all architectures to be compiled
-  if sys.platform == "darwin":
-    os.putenv("OBJARCH","-arch ppc -arch i386 -arch x86_64")
-
-  os.system("make static")
-  os.chdir(cwd)
+lib = ("hiredis", {
+  "sources": ["vendor/hiredis/%s.c" % src for src in ("hiredis", "net", "sds")],
+  "include_dirs": ["vendor/hiredis"]})
 
 ext = Extension("hiredis.hiredis",
   sources=glob.glob("src/*.c"),
   include_dirs=["src", "vendor"],
-  library_dirs=["vendor/hiredis"],
   libraries=["hiredis"])
 
 setup(
@@ -33,4 +27,5 @@ setup(
     keywords=["Redis"],
     license="BSD",
     packages=["hiredis"],
+    libraries=[lib],
     ext_modules=[ext])
