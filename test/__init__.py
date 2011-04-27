c1f5b9e3bfa96762fdbe9f4ca54b3851b38294da
Pieter Noordhuis <pcnoordhuis@gmail.com>
2011-04-27 10:44:47 +0200

    Add versioned path for dynamic library lookup

--- test/__init__.py
+++ test/__init__.py
@@ -1,7 +1,10 @@
 import glob, os.path, sys
 
+version = sys.version.split(" ")[0]
+majorminor = version[0:3]
+
 # Add path to hiredis.so load path
-path = glob.glob("build/lib*/hiredis/*.so")[0]
+path = glob.glob("build/lib*-%s/hiredis/*.so" % majorminor)[0]
 sys.path.insert(0, os.path.dirname(path))
 
 from unittest import *
