d272dee03f79a9ff378029e0a71a2cea22094f28
scott <juveduke@gmail.com>
2011-08-03 12:51:07 -0400

    Changed type checking to the builtin "PyList_CheckExact(obj)" instead of using "obj->ob_type == PyObj_Type"

--- src/reader.c
+++ src/reader.c
@@ -58,7 +58,7 @@ static void *tryParentize(const redisReadTask *task, PyObject *obj) {
     PyObject *parent;
     if (task && task->parent) {
         parent = (PyObject*)task->parent->obj;
-        assert(parent->ob_type == PyList_Type);
+        assert(PyList_CheckExact(parent));
         PyList_SET_ITEM(parent, task->idx, obj);
     }
     return obj;
