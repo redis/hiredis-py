75fd4aadedb4bcdcfe41f9ae61bf62282ffdadea
Pieter Noordhuis <pcnoordhuis@gmail.com>
2014-01-05 11:45:57 -0800

    Fix build path detection on SunOS
    Inside the hiredis directory there is another directory that contains the
    shared object. This is specific to the platform so we shouldn't care where the
    shared object itself is placed.

--- test/__init__.py
+++ test/__init__.py
@@ -4,8 +4,8 @@ version = sys.version.split(" ")[0]
 majorminor = version[0:3]
 
 # Add path to hiredis.so load path
-path = glob.glob("build/lib*-%s/hiredis/*.so" % majorminor)[0]
-sys.path.insert(0, os.path.dirname(path))
+path = glob.glob("build/lib*-%s/hiredis" % majorminor)[0]
+sys.path.insert(0, path)
 
 from unittest import *
 from . import reader
