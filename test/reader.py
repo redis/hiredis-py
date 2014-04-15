68ac439cb9c41d8efbbf98a538fb9ab3d7e22d47
Andy McCurdy <andy@andymccurdy.com>
2014-04-15 09:49:55 -0700

    use custom exception classes in tests to make sure callables work

--- test/reader.py
+++ test/reader.py
@@ -25,9 +25,12 @@ class ReaderTest(TestCase):
     self.assertRaises(RuntimeError, self.reply)
 
   def test_protocol_error_with_custom_callable(self):
-    self.reader = hiredis.Reader(protocolError=lambda err: RuntimeError(err))
+    class CustomException(Exception):
+      pass
+
+    self.reader = hiredis.Reader(protocolError=lambda e: CustomException(e))
     self.reader.feed(b"x")
-    self.assertRaises(RuntimeError, self.reply)
+    self.assertRaises(CustomException, self.reply)
 
   def test_fail_with_wrong_protocol_error_class(self):
     self.assertRaises(TypeError, hiredis.Reader, protocolError="wrong")
@@ -48,11 +51,14 @@ class ReaderTest(TestCase):
     self.assertEquals(("error",), error.args)
 
   def test_error_string_with_custom_callable(self):
-    self.reader = hiredis.Reader(replyError=lambda err: RuntimeError(err))
+    class CustomException(Exception):
+      pass
+
+    self.reader = hiredis.Reader(replyError=lambda e: CustomException(e))
     self.reader.feed(b"-error\r\n")
     error = self.reply()
 
-    self.assertEquals(RuntimeError, type(error))
+    self.assertEquals(CustomException, type(error))
     self.assertEquals(("error",), error.args)
 
   def test_fail_with_wrong_reply_error_class(self):
