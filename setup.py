a8d67768629ab15ea2e160fc2c5f7190837e0cbe
Michael Käufl <michael-k@users.noreply.github.com>
2018-09-08 23:54:13 +0200

    Update classifiers and add python_requires

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setup(
   # Override "install_lib" command
   cmdclass={ "install_lib": install_lib },
 
+  python_requires=">=2.6, !=3.0.*, !=3.1.*",
   classifiers=[
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
@@ -77,6 +78,9 @@ setup(
     'Programming Language :: Python :: 3.2',
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: Implementation :: CPython',
     'Topic :: Software Development',
   ],
