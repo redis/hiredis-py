901351a494614032a5979341f7444ffcd0021857
Yue Du <ifduyue@gmail.com>
2019-11-13 20:45:23 +0800

    Fix windows py38 appveyor incorrect parameter error

--- test/__init__.py
+++ test/__init__.py
@@ -3,10 +3,6 @@ import glob, os.path, sys
 version = sys.version.split(" ")[0]
 majorminor = version[0:3]
 
-# Add path to hiredis.so load path
-path = glob.glob("build/lib*-%s/hiredis" % majorminor)[0]
-sys.path.insert(0, path)
-
 from unittest import *
 from . import reader
 
