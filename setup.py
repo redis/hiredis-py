0675604f5414fef6c1d6573ac4558aaa5c2c1e12
Chayim I. Kirshen <c@kirshen.com>
2022-11-30 10:28:42 +0200

    Matching current supported python versions

--- setup.py
+++ setup.py
@@ -50,7 +50,6 @@ setup(
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
-    'Programming Language :: Python :: 3.11',
     'Programming Language :: Python :: Implementation :: CPython',
     'Topic :: Software Development',
   ],
